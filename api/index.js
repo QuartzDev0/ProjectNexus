3dacc6faa8975cc82f5259e23ba4499ca17c0c34
Quartz <hayesgiovanni53@gmail.com>
2025-11-10 21:24:20 -0500

    Delete api directory

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const SearchBasic = require("./search/basic.js")
-const SearchAdvanced = require("./search/advanced.js")
-
-const nexus = {
-  basic: SearchBasic.basic,
-  advanced: SearchAdvanced.advanced
-}
-
-module.exports = nexus
